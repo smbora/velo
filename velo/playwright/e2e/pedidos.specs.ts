8786ff49397db00b66b0766b4c8512e47b261037
Sarita Bora <saritabora@MacBook-Air-de-Sarita.local>
2026-06-02 10:17:17 -0300

    AAA - Arrange, Act, Assert

--- velo/playwright/e2e/pedidos.specs.ts
+++ velo/playwright/e2e/pedidos.specs.ts
@@ -1,17 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange
   await page.goto('http://localhost:5173/');
-  
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-OKP7BJ');
-  
   await page.getByTestId('search-order-button').click();
+
+  //Assert
+  await expect(page.getByTestId('order-result-id')).toBeVisible(); 
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-OKP7BJ');
+
+  await expect(page.getByTestId('order-result-status')).toBeVisible(); 
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+
 });
